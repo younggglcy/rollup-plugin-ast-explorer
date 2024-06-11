5ccf0f59e05e35132c32abd57d12ec21eea38c16
younggglcy <younggglcy@gmail.com>
2024-06-12 02:23:06 +0800

    feat: bundle asserts for ssr build and hydration

--- packages/rollup-plugin-ast-explorer/rollup.config.ts
+++ packages/rollup-plugin-ast-explorer/rollup.config.ts
@@ -1,9 +1,9 @@
 import { fileURLToPath } from 'node:url'
 import { dirname, resolve } from 'node:path'
 import { readFileSync } from 'node:fs'
+import { readdir } from 'node:fs/promises'
 import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
-import { nodeExternals } from 'rollup-plugin-node-externals'
 import { dts } from 'rollup-plugin-dts'
 import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
@@ -16,23 +16,33 @@ const pkgJsonPath = resolve(__dirname, 'package.json')
 
 const { version } = JSON.parse(readFileSync(pkgJsonPath, 'utf-8')) as { version: string }
 
+const stylesPath = resolve(__dirname, 'src/node/ssr/styles')
+
+const shared = [
+  commonjs(),
+  alias({
+    entries: [
+      {
+        find: /@\/(.*)/,
+        replacement: `${__dirname}/src/$1`,
+      },
+    ],
+  }),
+  typescript(),
+  nodeResolve(),
+]
+
 const mainConfig = defineConfig({
   input: 'src/index.ts',
   output: [
     {
-      // using dir here will cause an error (plugin typescript) RollupError:
-      // [plugin typescript] @rollup/plugin-typescript: Path of Typescript compiler option 'outDir' must be located inside Rollup 'dir' option.
-      // FYI: https://github.com/rollup/plugins/issues/1230#issuecomment-1324735742
-
-      // dir: 'dist/cjs',
-      file: 'dist/cjs/index.js',
+      dir: 'dist/cjs',
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
     },
     {
-      // dir: 'dist/esm',
-      file: 'dist/esm/index.js',
+      dir: 'dist/esm',
       format: 'esm',
       sourcemap: true,
     },
@@ -42,26 +52,13 @@ const mainConfig = defineConfig({
     tryCatchDeoptimization: false,
   },
   plugins: [
-    nodeResolve(),
-    commonjs(),
-    nodeExternals({
-      deps: false,
-    }),
-    typescript(),
+    ...shared,
     replace({
       include: ['./src/index.ts', './src/node/server/index.ts'],
       values: {
         __ROLLUP_PLUGIN_AST_EXPLORER_VERSION__: JSON.stringify(version),
       },
       preventAssignment: true,
-    }),
-    alias({
-      entries: [
-        {
-          find: /@\/(.*)/,
-          replacement: `${__dirname}/src/$1`,
-        },
-      ],
     }),
   ],
 })
@@ -77,4 +74,32 @@ const dtsConfig = defineConfig({
   ],
 })
 
-export default defineConfig([mainConfig, dtsConfig])
+const assertsConfig = defineConfig({
+  input: [
+    'src/client/bootstrap.tsx',
+  ],
+  output: {
+    dir: 'dist/asserts',
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    ...shared,
+    {
+      name: 'copy-and-watch-css',
+      buildStart() {
+        this.addWatchFile(stylesPath)
+      },
+      async generateBundle() {
+        const dir = await readdir(stylesPath)
+        Promise.all(dir.map(file => this.emitFile({
+          type: 'asset',
+          fileName: `styles/${file}`,
+          source: readFileSync(resolve(stylesPath, file)),
+        })))
+      },
+    },
+  ],
+})
+
+export default defineConfig([mainConfig, dtsConfig, assertsConfig])
--- packages/rollup-plugin-ast-explorer/src/node/server/router.tsx
+++ packages/rollup-plugin-ast-explorer/src/node/server/router.tsx
@@ -1,6 +1,8 @@
+import { resolve } from 'node:path'
 import { createRouter, defineEventHandler } from 'h3'
 import { renderToPipeableStream } from 'react-dom/server'
 import { App } from '../ssr/App'
+import { dirname } from '../constants'
 
 export const router = createRouter()
 
@@ -9,6 +11,9 @@ router.get(
   defineEventHandler((event) => {
     const { pipe } = renderToPipeableStream(
       <App />,
+      {
+        bootstrapScripts: [resolve(dirname, 'bootstrap.js')],
+      },
     )
     return pipe(event.node.res)
   }),
