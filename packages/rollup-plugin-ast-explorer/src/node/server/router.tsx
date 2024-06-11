5170701bd80283cc9a278b722365edc507e3a1f3
younggglcy <younggglcy@gmail.com>
2024-06-12 02:33:36 +0800

    feat: split ssr logic from server router

--- packages/rollup-plugin-ast-explorer/src/node/server/router.tsx
+++ packages/rollup-plugin-ast-explorer/src/node/server/router.tsx
@@ -1,20 +1,11 @@
-import { resolve } from 'node:path'
 import { createRouter, defineEventHandler } from 'h3'
-import { renderToPipeableStream } from 'react-dom/server'
-import { App } from '../ssr/App'
-import { dirname } from '../constants'
+import { pipe } from '../ssr/main'
 
 export const router = createRouter()
 
 router.get(
   '/',
   defineEventHandler((event) => {
-    const { pipe } = renderToPipeableStream(
-      <App />,
-      {
-        bootstrapScripts: [resolve(dirname, 'bootstrap.js')],
-      },
-    )
     return pipe(event.node.res)
   }),
 )
