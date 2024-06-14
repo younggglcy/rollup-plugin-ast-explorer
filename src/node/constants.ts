76224648ce62236dd8b0f009fc294c53223490c3
younggglcy <younggglcy@gmail.com>
2024-06-14 16:41:45 +0800

    feat: improve router logic, add ws client

--- src/node/constants.ts
+++ src/node/constants.ts
@@ -1,4 +1,4 @@
-import { dirname as dir } from 'node:path'
+import { dirname as dir, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 export const PORT = 7777
@@ -6,3 +6,4 @@ export const HOST = 'localhost'
 // shared because we bundle everything
 // all in one under dist/cjs and dist/esm
 export const dirname = dir(fileURLToPath(import.meta.url))
+export const assetsPath = resolve(dirname, '..', 'assets')
