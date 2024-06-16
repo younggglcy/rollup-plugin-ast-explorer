0e4eaea44c4a933f9da018946c586f6576aa5427
younggglcy <younggglcy@gmail.com>
2024-06-17 02:09:45 +0800

    fix: hydration error caused by top-level execution

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,5 @@ export const PORT = 7777
 export const HOST = 'localhost'
 // shared because we bundle everything
 // all in one under dist/cjs and dist/esm
-export const dirname = dir(fileURLToPath(import.meta.url))
-export const assetsPath = resolve(dirname, '..', 'assets')
+export const dirname = typeof window === 'undefined' ? dir(fileURLToPath(import.meta.url)) : ''
+export const assetsPath = typeof window === 'undefined' ? resolve(dirname, '..', 'assets') : ''
